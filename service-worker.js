9ffebdf643a56297ca17d10c70a0bb80e99a577d
oshiewhite <jcwhitedugan@gmail.com>
2026-09-05 12:10:01 -0400

    Refresh completed mobile Drive redirect

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'app-cache-v17';
+const CACHE_NAME = 'app-cache-v18';
 const MAX_NUMBERED_GEOJSON = 350;
 const OFFLINE_ASSETS = [
   'index.html', 'account.html', 'journal.html', 'manifest.json',
